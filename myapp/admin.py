69a848988f0abd0bdf396e6e963bea16ee762e5a
Shweta Arora <20bcs6704@cuchd.in>
2022-05-22 17:34:21 +0530

    Add files via upload

---
+++ myapp/admin.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from .models import Feature
+from .models import Student
+
+# Register your models here
+admin.site.register(Feature)
+
+admin.site.register(Student)
